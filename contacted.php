4f3a2a552dd1ce868bf53b315fc884b43a1564fd
Carlos Hinojosa <carloscience@gmail.com>
2013-10-03 14:02:27 -0700

    minor edits

---
+++ contacted.php
@@ -0,0 +1,8 @@
+<? include 'header.php' ?>
+<div class="ctext" style="padding-top:1.5em;">
+Thank you for contacting Jorin Bossen. We will get back to you as soon as we can.
+</div>
+</div>
+</div>
+</body>
+</html>
